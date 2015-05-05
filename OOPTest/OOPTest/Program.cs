1330010a50d596b76746beb204a197ee93524fc1
Aditya Wate <aditya.wate@hotmail.com>
2015-05-05 13:01:49 -0400

    C# Projects

---
+++ OOPTest/OOPTest/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTest
+{
+    /*
+    class RectanglePublic
+    {
+        //member variables
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            RectanglePublic r = new RectanglePublic();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+
+    class RectanglePrivate
+    {
+        //member variables
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            Console.Write("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            RectanglePrivate r = new RectanglePrivate();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+
+    class Rectangle
+    {
+        //member variables
+        internal double length;
+        internal double width;
+
+        double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+
+    class NumberManipulator
+    {
+        public void getValue(out int x)
+        {
+            int temp = 5;
+            x = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            NumberManipulator n = new NumberManipulator();
+
+            // local variable definition
+            int a = 100;
+
+            Console.WriteLine("Before method call, value of a : {0}", a);
+
+            // calling a function to get the value
+            n.getValue(out a);
+
+            Console.WriteLine("After method call, value of a : {0}", a);
+            Console.ReadLine();
+
+        }
+    }*/
+
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[3] {34, 67, 89}; /* n is an array of 10 integers */
+
+            foreach (int j in n)
+            {
+                //int i = j - 100;
+                //Console.WriteLine("Element[{0}] = {1}", i, j);
+                //i++;
+                Console.WriteLine("E[] = {0}",j);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestApps/TestApps/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApps
+{
+    /*
+    class SizeFind
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+            Console.ReadLine();
+        }
+    }
+
+    class ExplicitConversion
+    {
+        static void Main(string[] args)
+        {
+            double d = 5673.74;
+            int i;
+
+            // cast double to int.
+            Console.WriteLine("Before conversion it looks like {0}",d);
+            i = (int)d;
+            //i = d; build error
+            Console.WriteLine("Changing Ensues and now it's {0}",i);
+            Console.ReadKey();
+        }
+    } 
+
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            int i = 75;
+            float f = 53.005f;
+            double d = 2345.7652;
+            bool b = true;
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.ReadKey();
+
+        }
+    }
+
+    class Declaration
+    {
+        public static void Main(string[] args)
+        {
+            short a;
+            int b;
+            double c;
+
+            //initialization
+
+            a = 10;
+            b = 30;
+            c = a + b;
+
+            Console.WriteLine(" a = {0} \n b = {1} \n c = {2}", a, b, c);
+            Console.ReadLine();
+        }
+    }
+
+    class ConstDeclaration
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+
+            // constant declaration 
+            double r;
+            Console.WriteLine("Enter Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+            Console.ReadLine();
+        }
+    }
+
+    class OperatorsTest
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            a++;
+            Console.WriteLine("a = {0}", a);
+            Console.ReadLine();
+        }
+    }*/
+
+    class DecisionMaking
+    {
+        static void Main(string[] args)
+        {
+            // local variable definition
+            int a = 100;
+
+            // check the boolean condition
+            if (a < 20)
+            {
+                // if condition is true then print the following
+                Console.WriteLine("a is less than 20");
+            }
+            else
+            {
+                // if condition is false then print the following
+                Console.WriteLine("a is not less than 20");
+            }
+            Console.WriteLine("value of a is : {0}", a);
+
+
+            Work.TestNS tns = new Work.TestNS();
+
+            tns.Func();
+
+            Console.ReadLine();
+
+            
+        }
+    }
+
+    namespace Work
+    {
+        class TestNS
+        {
+            public void Func()
+            {
+                Console.Write("Jump");
+            }
+        }
+    }
+
+}
